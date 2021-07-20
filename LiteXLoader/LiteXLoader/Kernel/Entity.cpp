c534bd15d30a4f47dae4b8040a7ac480ece1ae31
littlegao233 <gaoxinhong2004@hotmail.com>
2021-07-20 20:45:49 +0800

    Update Entity.cpp

--- LiteXLoader/LiteXLoader/Kernel/Entity.cpp
+++ LiteXLoader/LiteXLoader/Kernel/Entity.cpp
@@ -5,69 +5,80 @@ using namespace std;
 
 string Raw_GetEntityName(Actor* actor)
 {
-    string name = actor->getNameTag();
-    return name.empty() ? Raw_GetEntityTypeName(actor) : name;
+	string name = actor->getNameTag();
+	return name.empty() ? Raw_GetEntityTypeName(actor) : name;
 }
 
 std::string Raw_GetEntityTypeName(Actor* actor)
 {
-    return SymCall("?EntityTypeToString@@YA?AV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@W4ActorType@@W4ActorTypeNamespaceRules@@@Z",
-        string, int, int)
-        (actor->getEntityTypeId(), 1);
+	return SymCall("?EntityTypeToString@@YA?AV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@W4ActorType@@W4ActorTypeNamespaceRules@@@Z",
+		string, int, int)
+		(actor->getEntityTypeId(), 1);
 }
 
 int Raw_GetEntityTypeId(Actor* actor)
 {
-    return actor->getEntityTypeId();
+	return actor->getEntityTypeId();
 }
 
 FloatVec4 Raw_GetEntityPos(Actor* actor)
 {
-    auto pos = actor->getPos();
-    return {pos.x,pos.y - 0.5f,pos.z,WActor(*actor).getDimID()};
+	auto pos = actor->getPos();
+	return { pos.x,pos.y - 0.5f,pos.z,WActor(*actor).getDimID() };
 }
 
-int Raw_GetHealth(Actor *actor)
+int Raw_GetHealth(Actor* actor)
 {
-    return SymCall("?getHealth@Actor@@QEBAHXZ", int, Actor*)(actor);
+	return SymCall("?getHealth@Actor@@QEBAHXZ", int, Actor*)(actor);
 }
 
-int Raw_GetMaxHealth(Actor *actor)
+int Raw_GetMaxHealth(Actor* actor)
 {
-    return SymCall("?getMaxHealth@Actor@@QEBAHXZ", int, Actor*)(actor);
+	return SymCall("?getMaxHealth@Actor@@QEBAHXZ", int, Actor*)(actor);
 }
 
-bool Raw_GetIsInAir(Actor *actor)
+bool Raw_GetIsInAir(Actor* actor)
 {
-    return !(dAccess<bool,448>(actor));
+	return !(dAccess<bool, 448>(actor));
 }
-
-bool Raw_TeleportEntity(Actor* actor, const FloatVec4 &pos)
+bool Raw_HasTag(Actor* actor, string* tag)
 {
-    WActor(*actor).teleport({pos.x,pos.y,pos.z},pos.dim);
-    return true;
+	return SymCall("?hasTag@Actor@@QEBA_NAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@@Z", bool, Actor*, std::string*)(actor, tag);
+}
+bool Raw_AddTag(Actor* actor, string* tag)
+{
+	return SymCall("?addTag@Actor@@QEAA_NAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@@Z", bool, Actor*, std::string*)(actor, tag);
+}
+bool Raw_RemoveTag(Actor* actor, string* tag)
+{
+	return SymCall("?removeTag@Actor@@QEAA_NAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@@Z", bool, Actor*, std::string*)(actor, tag);
+}
+bool Raw_TeleportEntity(Actor* actor, const FloatVec4& pos)
+{
+	WActor(*actor).teleport({ pos.x,pos.y,pos.z }, pos.dim);
+	return true;
 }
 
 bool Raw_KillEntity(Actor* actor)
 {
-    SymCall("?kill@Mob@@UEAAXXZ", void, void*)(actor);
-    return true;
+	SymCall("?kill@Mob@@UEAAXXZ", void, void*)(actor);
+	return true;
 }
 
 bool Raw_IsPlayer(Actor* actor)
 {
-    Player* pl = (Player*)actor;
-    auto plList = Raw_GetOnlinePlayers();
+	Player* pl = (Player*)actor;
+	auto plList = Raw_GetOnlinePlayers();
 
-    for (auto& player : plList)
-        if (player == pl)
-        {
-            return true;
-        }
-    return false;
+	for (auto& player : plList)
+		if (player == pl)
+		{
+			return true;
+		}
+	return false;
 }
 
 Player* Raw_ToPlayer(Actor* actor)
 {
-    return Raw_IsPlayer(actor) ? (Player*)actor : nullptr;
+	return Raw_IsPlayer(actor) ? (Player*)actor : nullptr;
 }
